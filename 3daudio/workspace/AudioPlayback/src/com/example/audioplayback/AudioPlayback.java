c9fb554dab68f7b1f4ddf5f1227fc77adc274ad2
Allison Card <acard@hmc.edu>
2013-02-13 18:54:49 -0800

    Tested and found that it is still too slow :-(

--- 3daudio/workspace/AudioPlayback/src/com/example/audioplayback/AudioPlayback.java
+++ 3daudio/workspace/AudioPlayback/src/com/example/audioplayback/AudioPlayback.java
@@ -9,46 +9,50 @@ public class AudioPlayback extends Activity
    @Override
    public void onCreate(Bundle savedInstanceState) 
    {
-      super.onCreate(savedInstanceState);                      
- 
-      new Thread( new Runnable( ) 
-      {
-         public void run( )
-         {        		
-            final float frequency = 440;
-            float increment = (float)(2*Math.PI) * frequency / 44100; // angular increment for each sample
-            float angle = 0;
-            AndroidAudioDevice device = new AndroidAudioDevice( );
-            float samples[] = new float[128];
-           	Audio3D audioModder = new Audio3D(samples, samples, 0, 5, 0, false);
-           	audioModder.init();
-           	// Up until this point, all data is what we think it should be
-            IRF_DATUM finalOut = new IRF_DATUM();
-            
-            while( true )
-            {
-            	for( int i = 0; i < samples.length; i++ )
-                {
-                   samples[i] = (float)FloatMath.sin(angle);
-                   angle += increment;
-                }
-            	
-               // Update position
-               audioModder.updateLocation(0.0, 0.0, 5.0); // Change this to actual changing values later
-               audioModder.updateInbuf(samples, samples);
-               // Instantiate Audio3D class and output datum
-           	//Audio3D audioModder = new Audio3D(samples, samples, azimGen.az, dist, el, false);
-               
-               // Futz the input sound 
-           	finalOut = audioModder.runAudio3D();
-              
-           	
-           	// Return final output
-              //device.writeSamples(finalOut.left );
- 
-               device.writeSamples( samples );
-            }        	
-         }
-      } ).start();
-   }
+		super.onCreate(savedInstanceState);
+
+		new Thread(new Runnable() {
+			public void run() {
+				final float frequency = 440;
+				float increment = (float) (2 * Math.PI) * frequency / 44100; // angular
+																				// increment
+																				// for
+																				// each
+																				// sample
+				float angle = 0;
+				AndroidAudioDevice device = new AndroidAudioDevice();
+				float samples[] = new float[128];
+				Audio3D audioModder = new Audio3D(samples, samples, 0, 5, 0,
+						false);
+				audioModder.init();
+				// Up until this point, all data is what we think it should be
+				IRF_DATUM finalOut = new IRF_DATUM();
+
+				while (true) {
+					for (int i = 0; i < samples.length; i++) {
+						samples[i] = (float) FloatMath.sin(angle);
+						angle += increment;
+					}
+
+					// Update position
+					audioModder.updateLocation(0.0, 0.0, 5.0); // Change this to
+																// actual
+																// changing
+																// values later
+					audioModder.updateInbuf(samples, samples);
+					// Instantiate Audio3D class and output datum
+					// Audio3D audioModder = new Audio3D(samples, samples,
+					// azimGen.az, dist, el, false);
+
+					// Futz the input sound
+					finalOut = audioModder.runAudio3D();
+
+					// Return final output
+					// device.writeSamples(finalOut.left );
+
+					device.writeSamples(samples);
+				}
+			}
+		}).start();
+	}
 }
